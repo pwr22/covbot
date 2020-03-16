57dd9f7da868d874dc390fdc5327b1e618a79cce
Peter Roberts <peter@shortestpath.dev>
2020-03-16 23:20:36 +0000

    Refactor.

--- covbot.py
+++ covbot.py
@@ -9,6 +9,7 @@ import pprint
 import datetime
 import asyncio
 import whoosh
+import time
 from whoosh.fields import Schema, TEXT
 from whoosh.index import create_in, FileIndex
 from whoosh.qparser import QueryParser
@@ -28,7 +29,7 @@ class CovBot(Plugin):
     async def _get_country_groups(self):
         groups = {}
 
-        self.log.debug("fetching %s", GROUPS_URL)
+        self.log.debug("Fetching %s.", GROUPS_URL)
         async with self.http.get(GROUPS_URL) as r:
             t = await r.text()
 
@@ -41,8 +42,9 @@ class CovBot(Plugin):
 
     async def _get_case_data(self):
         countries = {}
+        now = time.time() * 1000  # millis to match the data
 
-        self.log.debug("fetching %s", CASE_DATA_URL)
+        self.log.debug("Fetching %s.", CASE_DATA_URL)
         l = None
         async with self.http.get(CASE_DATA_URL) as r:
             t = await r.text()
@@ -52,6 +54,7 @@ class CovBot(Plugin):
         cr = csv.DictReader(l, delimiter=';')
         for row in cr:
             country = row['Country']
+
             if not country in countries:
                 countries[country] = {'totals': {
                     'cases': 0, 'recoveries': 0, 'deaths': 0}, 'areas': {}}
@@ -59,22 +62,26 @@ class CovBot(Plugin):
             cases, deaths, recoveries = (0 if n == '' else int(n) for n in (
                 row[k] for k in ('Confirmed', 'Deaths', 'Recovered')))
 
-            epoch = int(int(row['LastUpdated']) / 1000)
-            last_update = datetime.datetime.utcfromtimestamp(epoch)
+            ts = int(int(now if row['LastUpdated']
+                         == '' else row['LastUpdated']) / 1000)
+            last_update = datetime.datetime.utcfromtimestamp(ts)
 
             area = row['Province']
             if area == '':
-                countries[country]['totals'] = {
-                    'cases': cases, 'deaths': deaths, 'recoveries': recoveries, 'last_update': last_update}
+                d = {'cases': cases, 'deaths': deaths,
+                     'recoveries': recoveries, 'last_update': last_update}
+                countries[country]['totals'] = d
             else:
-                countries[country]['areas'][area] = {
-                    'cases': cases, 'deaths': deaths, 'recoveries': recoveries, 'last_update': last_update}
+                d = {'cases': cases, 'deaths': deaths,
+                     'recoveries': recoveries, 'last_update': last_update}
+                countries[country]['areas'][area] = d
 
         return countries
 
     def _update_index(self):
         # create a new index
         d = '/tmp/covbotindex'
+        self.log.debug('Updating index in %s', d)
         if not os.path.exists(d):
             os.mkdir(d)
 
@@ -95,43 +102,37 @@ class CovBot(Plugin):
         now = datetime.datetime.utcnow()
 
         if self.next_update_at == None or now >= self.next_update_at:
-            self.log.info('updating data')
+            self.log.info('Updating data.')
             self.groups, self.cases = await asyncio.gather(self._get_country_groups(), self._get_case_data())
-
-            # one we've got the data we can update the index
-            loop = asyncio.get_running_loop()
-            await loop.run_in_executor(None, lambda: self._update_index())
-
+            await asyncio.get_running_loop().run_in_executor(None, self._update_index)
             self.next_update_at = now + datetime.timedelta(minutes=15)
         else:
-            self.log.info('too early to update - using cached data')
+            self.log.info('Too early to update - using cached data.')
 
     def _exact_country_match(self, query: str) -> list:
+        self.log.info('Trying an exact country match on %s', query)
         for country in self.cases:
             if country.lower() == query.lower():
+                self.log.debug('Got an exact country match on %s', query)
                 return ((country, self.cases[country]['totals']),)
 
         return None
 
     def _exact_region_match(self, query: str) -> list:
+        self.log.info('Trying an exact region match on %s', query)
         regions = []
         for country, data in self.cases.items():
             for area, data in data['areas'].items():
                 if area.lower() == query.lower():
                     regions.append((f'{area}, {country}', data))
 
+        if len(regions) > 0:
+            self.log.debug('Got exact region matches on %s: %s', query, regions)
+
         return regions
 
-    def _get_data_for(self, query: str) -> list:
-        m = self._exact_country_match(query)
-        if m != None:
-            return m
-
-        areas = self._exact_region_match(query)
-        if len(areas) > 0:
-            return areas
-
-        # try wildcard location match
+    def _wildcard_location_match(self, query: str) -> list:
+        self.log.info('Trying a wildcard location match on %s', query)
         with self.index.searcher() as s:
             qs = f'*{query}*'
             q = QueryParser("location", self.schema).parse(qs)
@@ -149,7 +150,24 @@ class CovBot(Plugin):
                 locs.append((l, d))
 
             if len(locs) > 0:
-                return locs
+                self.log.debug('Found wildcard location matches on %s: %s', query, locs)
+
+            return locs
+
+    def _get_data_for(self, query: str) -> list:
+        self.log.info('Looking up data for %s', query)
+
+        m = self._exact_country_match(query)
+        if m != None:
+            return m
+
+        areas = self._exact_region_match(query)
+        if len(areas) > 0:
+            return areas
+
+        locs = self._wildcard_location_match(query)
+        if len(locs) > 0:
+            return locs
 
         return []
 
@@ -159,16 +177,23 @@ class CovBot(Plugin):
         if location == "":
             location = "World"
 
+        self.log.info('Responding to cases request for %s.', location)
+
         try:
             await self._update_data()
         except Exception as e:
-            self.log.error('failed to update data: %s', e)
+            self.log.warn('Failed to update data: %s.', e)
             await event.respond('Something went wrong fetching the latest data so stats may be outdated.')
 
         matches = await asyncio.get_running_loop().run_in_executor(None, self._get_data_for, location)
+        # matches = self._get_data_for(location)
 
         if len(matches) == 0:
-            await event.respond(f'I have searched my data but cannot find a match for {location}. It might be under a different name or there may be no cases! If I am wrong let @pwr22:shortestpath.dev know.')
+            await event.respond(
+                f'I have searched my data but cannot find a match for {location}.'
+                ' It might be under a different name or there may be no cases!'
+                ' If I am wrong let @pwr22:shortestpath.dev know.'
+            )
             return
         elif len(matches) > 1:
             ms = " - ".join(m[0] for m in matches)
@@ -193,14 +218,16 @@ class CovBot(Plugin):
 
     @command.new('source', help='Get my source code and the data I use.')
     async def source_handler(self, event: MessageEvent) -> None:
-        await event.respond((
+        self.log.info('Responding to source request.')
+        await event.respond(
             'I am MIT licensed on Github at https://github.com/pwr22/covbot.'
             f' I fetch new data every 15 minutes from {CASE_DATA_URL}.'
-        ))
+        )
 
     # TODO make less clever and one line per command
     @command.new('help', help='Get usage help using me.')
     async def help_handler(self, event: MessageEvent) -> None:
+        self.log.info('Responding to help request.')
         for h in self.cases_handler, self.source_handler, self.help_handler:
             s = h.__mb_full_help__ + ' - ' + h.__mb_help__
             await event.respond(s)
